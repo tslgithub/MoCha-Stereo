f96f82b3bae0afc4900bb24237f13027976fc8ea
tslgithub <tiansonglu@gmail.com>
2025-01-07 15:03:37 +0800

    Update run.sh

--- MoCha-Stereo/run.sh
+++ MoCha-Stereo/run.sh
@@ -4,7 +4,7 @@
 # https://github.com/ZYangChen/MoCha-Stereo/releases
 
 python3 demo.py \
-	--left_imgs '/mnt/Data2/depth/depth20250102/left2/*.png' \
+  --left_imgs '/mnt/Data2/depth/depth20250102/left2/*.png' \
   --right_imgs '/mnt/Data2/depth/depth20250102/right2/*.png' \
   --restore_ckpt ./weights/mocha-stereo.pth \
   --output_directory /mnt/Data2/depth/depth20250102/result/MoCha-Stereo/
