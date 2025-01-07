977fe7a389e69f1530a94b4739e1c6af62b029d4
tslgithub <tiansonglu@gmail.com>
2025-01-07 14:51:08 +0800

    Create run.sh

---
+++ MoCha-Stereo/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+python3 demo.py \
+	--left_imgs '/mnt/Data2/depth/depth20250102/left2/*.png' \
+  --right_imgs '/mnt/Data2/depth/depth20250102/right2/*.png' \
+  --restore_ckpt ./weights/mocha-stereo.pth \
+  --output_directory /mnt/Data2/depth/depth20250102/result/MoCha-Stereo/
